e46989865b1d640376b2db7025d962a4bd99a147
Ori Levy <ori294@gmail.com>
2020-01-13 22:52:21 +0200

    Added A star Algorithm

--- ex4/SearchAlgorithms/BreadthFirstSearch.h
+++ ex4/SearchAlgorithms/BreadthFirstSearch.h
@@ -2,117 +2,66 @@
 // Created by ori294 on 1/10/20.
 //
 
-/*#ifndef EX4_SEARCHALGORITHMS_BREADTHFIRSTSEARCH_H_
+#ifndef EX4_SEARCHALGORITHMS_BREADTHFIRSTSEARCH_H_
 #define EX4_SEARCHALGORITHMS_BREADTHFIRSTSEARCH_H_
 
-#include <string>
-#include <SearchAlgorithms/Searcher.h>
-#include <vector>
+#include "Searcher.h"
 #include <queue>
-#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
 
-template<class T, class Solution>
-class BreadthFirstSearch : Searcher<T, Solution> {
-
+ template<class Solution, class T>
+class BreadthFirstSearch : public Searcher<Solution, T> {
  private:
-  Searchable<T>* searchable;
-  double size;
+  Solution solution_;
+  bool is_path_exist = false;
+  int evaluatedNodes = 0;
+  std::set<State<T>*> visited;
+  std::queue<State<T>*> bfs_queue;
 
  public:
-  Solution search(Searchable<T>* toSearch) {
-    this->searchable = toSearch;
-    this->size = searchable->getSize();
-    Solution sol = bfs(this->searchable->GetInitialState(),this->searchable->GetGoalState());
-    return sol;
+  Solution search(Searchable<T> *searchable) override {
+    searchable->GetInitialState()->setCameFrom(nullptr);
+    searchable->GetInitialState()->setCost(0);
+    bfs(searchable->GetInitialState(), searchable->GetGoalState(), searchable);
+    if (is_path_exist) {
+      solution_ = searchable->Dynamic_programming_recovery();
+    }
+    return this->solution_;
   }
 
-  int getNumberOfNodesEvaluated() override {
+  void bfs(State<T> *start, State<T> *end, Searchable<T> *searchable) {
 
-  }
+    visited.insert(start);
+    bfs_queue.push(start);
+    while (!bfs_queue.empty()) {
+      State<T>* vertex = bfs_queue.front();
+      bfs_queue.pop();
 
+      if (vertex->Equals(end)) {
+        is_path_exist = true;
+        return;
+      }
 
-  Solution bfs(State<T>start , State<T> end){
-    std::vector<std::vector<bool>> visited;
-    std::vector<std::vector<double>> distances;
-
-    std::vector<bool> tempVisited;
-    std::vector<double> tempDistances;
-    for (int i = 0; i < size; i++) {
-      tempVisited.emplace_back(false);
-      tempDistances.emplace_back(0);
-    }
-    for (int i = 0; i < size; i++) {
-      visited.emplace_back(tempVisited);
-      distances.emplace_back(tempDistances);
-    }
-
-
-
-    std::queue<State<T>> searchQueue;
-    int sourceIndexX = 0, sourceIndexY = 0;
-    int targetIndexX = 0, targetIndexY = 0;
-
-    for (int i = 0; i < size; i++) {
-      for (int j = 0; j < size; j++) {
-        visited[i][j] = (searchable->getStateByIndex(i, j).getCost() == -1); //-1 for infinity distance (temp)
-        if (start.getCol() == j && start.getRow() == i) {
-          std::cout << "Found start index" << std::endl;
-          sourceIndexX = i;
-          sourceIndexY = j;
+      std::list<State<T>*> adjacent = searchable->GetAllPossibleStates(vertex);
+      for (State<T>* adj:adjacent) {
+        if (adj->getCost() == -1) { //-1 for infinity, IDK how they gonna represent it
+          visited.insert(adj);
+        }
+        if (!visited.count(adj)) {
+          visited.insert(adj);
+          adj->setCameFrom(vertex);
+          adj->setCost(vertex->getCost() + 1);
+          bfs_queue.push(adj);
+          evaluatedNodes++;
         }
       }
     }
-
-    searchQueue.push(searchable->getStateByIndex(sourceIndexX, sourceIndexY));
-    distances[start.getRow()][start.getCol()] = 0;
-    visited[sourceIndexX][sourceIndexY] = true;
-
-    while (!searchQueue.empty()) {
-      State<T> vertex = searchQueue.front();
-      searchQueue.pop();
-
-      if (vertex.Equals(end)) {
-        std::cout << "Target found" << std::endl;
-        return Solution(distances[vertex.getRow()][vertex.getCol()]);
-      }
-
-      //scan upside
-      if (vertex.getRow() > 0 && !visited[vertex.getRow()-1][vertex.getCol()]) {
-        searchQueue.push(searchable->getStateByIndex(vertex.getRow() - 1, vertex.getCol()));
-
-        visited[vertex.getRow() - 1][vertex.getCol()] = true;
-        distances[vertex.getRow() - 1][vertex.getCol()] = distances[vertex.getRow()][vertex.getCol()] + 1;
-      }
-
-      //scan downside
-      if (vertex.getRow() < size - 1 && !visited[vertex.getRow()+1][vertex.getCol()]) {
-        searchQueue.push(searchable->getStateByIndex(vertex.getRow() + 1, vertex.getCol()));
-
-        visited[vertex.getRow() + 1][vertex.getCol()] = true;
-        distances[vertex.getRow() + 1][vertex.getCol()] = distances[vertex.getRow()][vertex.getCol()] + 1;
-      }
-
-      //scan rightside
-      if (vertex.getCol() < size && !visited[vertex.getRow()][vertex.getCol() + 1]) {
-        searchQueue.push(searchable->getStateByIndex(vertex.getRow(), vertex.getCol() + 1));
-
-        visited[vertex.getRow()][vertex.getCol() + 1] = true;
-        distances[vertex.getRow()][vertex.getCol() + 1] = distances[vertex.getRow()][vertex.getCol()] + 1;
-      }
-
-      //scan leftside
-      if (vertex.getCol() > 0 && !visited[vertex.getRow()][vertex.getCol()-1]) {
-        searchQueue.push(searchable->getStateByIndex(vertex.getRow(), vertex.getCol() - 1));
-
-        visited[vertex.getRow()][vertex.getCol() - 1] = true;
-        distances[vertex.getRow()][vertex.getCol() - 1] = distances[vertex.getRow()][vertex.getCol()] + 1;
-      }
-
-    }
-
   }
 
+  int getNumberOfNodesEvaluated() override {
+    return evaluatedNodes;
+  }
 };
-
 #endif //EX4_SEARCHALGORITHMS_BREADTHFIRSTSEARCH_H_
-*/
